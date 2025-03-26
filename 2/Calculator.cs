426c056e8175756c29e1f111de1ea5d244e32bb2
Long32 <99838967+Long132000@users.noreply.github.com>
2025-03-26 11:41:11 +0300

    Add files via upload

---
+++ 2/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    public class Calculator
+    {
+        /// <summary>
+        /// Вычисляет Z по заданным x и y.
+        /// </summary>
+        /// <param_name="x">Первое число</param>
+        /// <param_name="y">Второе число</param>
+        /// <returns>Результат Z</returns>
+        /// <exception_cref="ArgumentException">Если x или y не являются числами</exception>
+        public static double CalculateZ(double x, double y)
+        {
+            if (x < 0)
+            {
+                return Math.Max(x, y);
+            }
+            else
+            {
+                return Math.Min(x, y);
+            }
+        }
+    }
+}
---
+++ 2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ButtonCalculate_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                double x = double.Parse(textBoxX.Text);
+                double y = double.Parse(textBoxY.Text);
+                double z = Calculator.CalculateZ(x, y);
+                labelResult.Text = $"Z = {z}";
+            }
+            catch (FormatException)
+            {
+                labelResult.Text = "Ошибка: введите числа!";
+            }
+            catch (Exception ex)
+            {
+                labelResult.Text = $"Ошибка: {ex.Message}";
+            }
+        }
+    }
+}
